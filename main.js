e740dd8b043d208f1b55c36f366d072e235092ed
hanskopeyy <hans.hadiwijaya@binus.ac.id>
2024-04-10 21:17:20 +0700

    Fixed Import bug

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three';
+import * as THREE from './node_modules/three/build/three.module.js';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { FontLoader } from './node_modules/three/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from './node_modules/three/examples/jsm/geometries/TextGeometry.js';
